530cc8eb6d7079bfbdba7986083460b462849c9b
Tevfik <tevfikozlen3@gmail.com>
2025-04-12 23:27:41 -0400

    Complete Phase 1

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/sessions">Sessions</Link></li>
+        <li><Link href="/goals">Goals</Link></li>
+      </ul>
+    </nav>
+  );
+}
